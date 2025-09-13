3011d7996b52ad67c51c16a4c8858b4c295be27d
tusharsaste17 <tusharbsaste@gmail.com>
2025-09-14 01:26:27 +0530

    test commit

--- MyAccountService/src/main/java/com/example/myaccount/controller/AccountController.java
+++ MyAccountService/src/main/java/com/example/myaccount/controller/AccountController.java
@@ -17,10 +17,10 @@ public class AccountController {
 
     private IAccountsService iAccountsService;
 
-    @GetMapping("/hello")
+   /* @GetMapping("/hello")
     public String sayHello(){
         return "hello world..!!";
-    }
+    }*/
 
     @PostMapping("/createAccount")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
