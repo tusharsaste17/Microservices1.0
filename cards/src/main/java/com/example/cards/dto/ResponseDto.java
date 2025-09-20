aadc5bde03483077de45c7c000e154a29ee209d3
tusharsaste17 <tusharbsaste@gmail.com>
2025-09-20 15:13:12 +0530

    add new projects cards and loans in repo

---
+++ cards/src/main/java/com/example/cards/dto/ResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.cards.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data @AllArgsConstructor
+public class ResponseDto {
+
+
+    private String statusCode;
+
+    private String statusMsg;
+    
+}
